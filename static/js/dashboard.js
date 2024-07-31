e035502ef28f4e99d889b61e219dd990561ed4eb
apalaciosgoldco <apalaciosgoldco@gmail.com>
2024-07-31 00:32:02 -0700

    a

--- static/js/dashboard.js
+++
@@ -1,7 +0,0 @@
-// dashboard.js
-$(document).ready(function() {
-    console.log('Dashboard JS loaded.');
-    
-    // Example: Add any JS functionality you need here
-    $('#dynamic-content').html('<p>Welcome to the dashboard!</p>');
-});
