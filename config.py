2acb0bb16adfaafaa03ee73911d02035b5b8fbfc
ReverseAlfred <149822612+ReverseAlfred@users.noreply.github.com>
2024-10-04 11:58:15 -0600

    UpdateACCOUNT

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # Snowflake connection settings with environment variable overrides
-    SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT', 'PRBBFDD-FF31368')
+    SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT', 'CMWDBMC-HB15746')
     SNOWFLAKE_WAREHOUSE = os.getenv('SNOWFLAKE_WAREHOUSE', 'NEWCKB_WH')
     SNOWFLAKE_DATABASE = os.getenv('SNOWFLAKE_DATABASE', 'NEWCKB')
     SNOWFLAKE_SCHEMA = os.getenv('SNOWFLAKE_SCHEMA', 'public')
